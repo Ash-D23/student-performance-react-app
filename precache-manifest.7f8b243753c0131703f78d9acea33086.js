421e0a99de9ed76fe1e2bd1867da1f2606ba1c6f
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2021-10-30 11:24:54 +0530

    Updates

---
+++ precache-manifest.7f8b243753c0131703f78d9acea33086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "94351dfd560053d9aa75",
+    "url": "/student-performance-react-app/static/css/main.03ee819d.chunk.css"
+  },
+  {
+    "revision": "94351dfd560053d9aa75",
+    "url": "/student-performance-react-app/static/js/main.dbf15ce1.chunk.js"
+  },
+  {
+    "revision": "184c7821b179a480e5ec",
+    "url": "/student-performance-react-app/static/js/runtime~main.b86f81b2.js"
+  },
+  {
+    "revision": "b3070aaa9a46dada5af2",
+    "url": "/student-performance-react-app/static/css/2.2b868ebf.chunk.css"
+  },
+  {
+    "revision": "b3070aaa9a46dada5af2",
+    "url": "/student-performance-react-app/static/js/2.04115ade.chunk.js"
+  },
+  {
+    "revision": "a15b287fa42ba4c738bc68a07e865e61",
+    "url": "/student-performance-react-app/index.html"
+  }
+];
